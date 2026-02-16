85e0683a92e2d33dfd885adc28c3ae66fbf79689
Stefan Fourie <stefanfourie001@gmail.com>
2026-02-16 16:56:20 +0200

    Added Register Page

--- doctor_website_demo/src/App.tsx
+++ doctor_website_demo/src/App.tsx
@@ -3,15 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from "./pages/Home"
 import Login from "./pages/Login"
+import Register from './pages/Register'
 import './index.css'
+import Navbar from "./components/Navbar"
 import { Routes, Route } from "react-router-dom"
 
 function App() {
  return(
+  <>
+    <Navbar />
    <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
+  </>
+  
  )
 }
 
--- doctor_website_demo/src/pages/Register.tsx
+++ doctor_website_demo/src/pages/Register.tsx
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"
 
+function Register(){
+    const [firstname, setFirstname] = useState("");
+    const [lastname , setLastname] = useState("");
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
 
-function Login(){
-    const [email,setEmail] = useState("");
-    const [password, setPassword] = useState("")
-
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = (e: React.FormEvent) =>{
         e.preventDefault()
-        console.log("Email: " , email)
-        console.log("Password: ", password)
-        alert("Login Submitted")
+        
     }
 
     return(
@@ -18,6 +16,22 @@ function Login(){
             <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
                 <h2 className="text-3xl font-bold text-center mb-6">Welcome Back!</h2>
                 <form onSubmit={handleSubmit} className="space-y-5">
+                    <div>
+                        <label className="block mb-2 text-sm font-md">
+                            First Name
+                        </label>
+                        <input type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"/>
+                        
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-md">
+                            Password
+                        </label>
+                        <input type="text" value={lastname} onChange={(e) => setLastname(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"/>
+                        
+                    </div>
                     <div>
                         <label className="block mb-2 text-sm font-md">
                             Email
@@ -36,32 +50,20 @@ function Login(){
                         onChange={(e) => setPassword(e.target.value)}
                         className="w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"/>
                     </div>
-                    <div className="flex justify-between items-center text-sm">
-                        <label className="flex items-center gap-2">
-                            Remember Me
-                            <input type="checkbox" />
-                        </label>
-                        <a href="" className="hover:underline">
-                            Forgot Password?
-                        </a>
-                    </div>
+                   
 
                     <button
                     type="submit"
                     className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-grey-500 transition">
-                        Login
+                        Register
                     </button>
                 </form>
 
-                <div>
-                    <p className="text-center text-sm">Dont have an account?
-                        <a href="" className="hover:underline">Sign Up Here!</a>
-                        <Link to="/register" className="hover:underline">Register</Link>
-                    </p>
-                </div>
+                
             </div>
         </div>
+
     )
 }
 
-export default Login;
+export default Register;
